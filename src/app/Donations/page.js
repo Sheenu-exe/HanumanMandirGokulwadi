876bea2aa27fd6c4fbb2c5c5d074785f049ce541
Sachin Parihar <pariharsachin5002@gmail.com>
2024-05-13 17:15:48 +0530

    qr

--- src/app/Donations/page.js
+++ src/app/Donations/page.js
@@ -7,17 +7,22 @@ const Donation = () => {
     <Layout>
       <div className="h-screen flex"> 
         <div className="w-[50%] h-screen flex flex-col justify-center ">
+          <div className="w-full flex items-center justify-center">
+        <img className='h-[45vh] w-[31vh]' src="https://i.ibb.co/4p5LVTc/hanumanji-gys4-Photoroom-png-Photoroom.png" alt="" /></div>
             <p className="text-3xl m-3 font-bold">आपके उदार दान से श्री गोकुलवाड़ी हनुमान मंदिर का समर्थन करें|</p>
             <p className="text-xl m-3 mt-0">आपके दान से मंदिर का विकास और रखरखाव संभव होगा।</p>
             <p className="text-lg mx-3">दान कैसे करें:</p>
             <p className="mx-3">
-            ऑनलाइन भुगतान : फॉर्म द्वारा<br/>
-            बैंक हस्तांतरण : (खाता विवरण दिया गया है)<br/>
-चेक/नकद : गोकुलवाड़ी गरबा चौक, शिवगंज, सिरोही - 307027
+            दिए गए क्यू आर कोड को स्कैन कर अपनी मनोवांछित राशि दान करे
+इस व्हाट्सएप क्रमांक पर अपने भुगतान का स्क्रीनशॉट भेजे 
+व दिए गए अपलोड बटन पे क्लिक कर अपलोड करे | 
             </p>
+            
         </div>
-        <div className="w-[50%] h-screen">
-          form
+        <div className="w-[50%] h-screen flex justify-center items-center">
+         <div className="h-[50vh] w-auto">
+          <img className="w-full h-full" src="https://i.ibb.co/w76S28S/UPI.jpg"/>
+         </div>
         </div>
       </div>
       <marquee
